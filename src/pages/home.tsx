12788379eee6d937c3926706e2247e3b965fbec7
ducanhgm14102005 <ducanhgm14102005@gmail.com>
2026-03-23 22:00:44 +0700

    Update home

---
+++ src/pages/home.tsx
@@ -0,0 +1,72 @@
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
+import ProductCard from "../../components/ProductCard/ProductCard";
+import OrderModal from "../../components/Modal/OrderModal";
+
+import namDong from "../../assets/images/Nam-dong.png";
+import tauHu from "../../assets/images/Tau-hu-chien.png";
+
+const Home = () => {
+  const products = [
+    {
+      name: "Nấm sốt đông",
+      price: 20000,
+      image: namDong,
+    },
+    {
+      name: "Tàu hũ chiên giòn",
+      price: 30000,
+      image: tauHu,
+    },
+  ];
+
+  return (
+    <>
+      <Header />
+
+      <div className="main">
+        <div className="container">
+          {/* Slide */}
+          <div className="slide_show">
+            <button className="prev_btn">
+              <i className="fa-solid fa-chevron-left"></i>
+            </button>
+
+            <img src={namDong} alt="slide" />
+
+            <button className="next_btn">
+              <i className="fa-solid fa-chevron-right"></i>
+            </button>
+          </div>
+
+          {/* Service */}
+          <div className="slide_service">
+            <div className="service_item">
+              <i className="fa-solid fa-truck-fast"></i>
+              <div>
+                <h3>Giao hàng tận nơi</h3>
+                <p>Nhanh chóng</p>
+              </div>
+            </div>
+          </div>
+
+          {/* Product */}
+          <div className="product">
+            <h2 className="product_title">Món Chay</h2>
+
+            <div className="productContainer">
+              {products.map((item, index) => (
+                <ProductCard key={index} product={item} />
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <Footer />
+      <OrderModal />
+    </>
+  );
+};
+
+export default Home;
