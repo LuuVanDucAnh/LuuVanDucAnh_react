a9bc468544c59021620ffa7a9ef093e42a08ff09
ducanhgm14102005 <ducanhgm14102005@gmail.com>
2026-03-23 23:34:49 +0700

    Update inport NamDong

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,8 +3,8 @@ import Footer from "../components/Footer";
 import ProductCard from "../components/ProductCard";
 import OrderModal from "../components/Modal";
 
-import namDong from "../assets/";
-import tauHu from "../../assets/images/Tau-hu-chien.png";
+import namDong from "../assets/css/images/Nam-dong.png";
+import tauHu from "../assets/css/images/Tau-hu-chien.png";
 
 const Home = () => {
   const products = [
