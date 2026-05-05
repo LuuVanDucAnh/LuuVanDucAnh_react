c7002d6a75d6804dc13dd69e7c2ff61d55d14403
ducanhgm14102005 <ducanhgm14102005@gmail.com>
2026-05-05 14:19:01 +0700

    Fix lỗi khi giao diện không import được với css

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,17 @@
 /// <reference types="react-scripts" />
+
+declare module "*.css" {
+  const content: { [className: string]: string };
+  export default content;
+}
+
+declare module "*.scss" {
+  const content: { [className: string]: string };
+  export default content;
+}
+
+declare module "*.png";
+declare module "*.jpg";
+declare module "*.jpeg";
+declare module "*.svg";
+declare module "*.gif";
